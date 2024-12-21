fa5e5ed57c6e9a0abd678c6eb10da5b6e605ec9a
Marlos Ribeiro <550561+mgrb@users.noreply.github.com>
2024-12-21 16:10:04 -0300

    add Some aliases

--- .zsh/config/aliases.zsh
+++ .zsh/config/aliases.zsh
@@ -1,2 +1,9 @@
 alias ls="eza --icons --group-directories-first -l"
 alias ll="eza --icons --group-directories-first -la"
+
+alias cp='cp -iv'                           # Preferred 'cp' implementation
+alias mv='mv -iv'                           # Preferred 'mv' implementation
+alias mkdir='mkdir -pv'                     # Preferred 'mkdir' implementation
+cd() { builtin cd "$@"; ll; }               # Always list directory contents upon 'cd'
+alias edit='subl'                           # edit:         Opens any file in sublime editor
+mcd () { mkdir -p "$1" && cd "$1"; }        # mcd:          Makes new Dir and jumps inside
