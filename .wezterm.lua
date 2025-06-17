ca6ee59b217688acda0e0dae381163e1783f7f91
Marlos Ribeiro <550561+mgrb@users.noreply.github.com>
2025-06-17 11:07:47 -0300

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -12,11 +12,21 @@ config.line_height = 1.1
 
 -- disable tab & Window boards
 config.enable_tab_bar = false
-config.window_decorations = "NONE"
+config.window_decorations = "TITLE | RESIZE"
 
 -- For example, changing the color scheme:
 config.color_scheme = 'SeaShells'
-config.window_background_opacity = 0.9
+-- config.window_background_opacity = 0.9
+
+-- Recuperando o hostname
+local hostname = wezterm.hostname()
+-- Recuperando o username
+local username = os.getenv("USER") or os.getenv("USERNAME") or "unknown"
+
+-- Seta o titulo da janela
+wezterm.on('format-window-title', function(tab, pane, tabs, panes, config)
+  return string.format("%s :: %s@%s", tab.tab_id, username, hostname)
+end)
 
 -- and finally, return the configuration to wezterm
 return config
